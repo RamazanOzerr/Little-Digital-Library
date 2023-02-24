f884f93931d7e477a81bc5054ba2d901f5955c28
Ramazan Özer <ozerramazan991@gmail.com>
2023-02-24 16:18:44 +0300

    DownloadedBooksAdapter and DownloadedBooksFragment been added

--- app/src/main/java/com/example/spring_android_project/Fragments/DowloadedBooksFragment.java
+++ app/src/main/java/com/example/spring_android_project/Fragments/DowloadedBooksFragment.java
@@ -3,64 +3,55 @@ package com.example.spring_android_project.Fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.spring_android_project.Adapters.AvailableBooksAdapter;
+import com.example.spring_android_project.Adapters.DownloadedBooksAdapter;
 import com.example.spring_android_project.R;
+import com.example.spring_android_project.Utils.Book;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DowloadedBooksFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class DowloadedBooksFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DowloadedBooksFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DowloadedBooksFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DowloadedBooksFragment newInstance(String param1, String param2) {
-        DowloadedBooksFragment fragment = new DowloadedBooksFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private List<Book> bookList;
+    private RecyclerView recyclerView_downloaded_books;
+    DownloadedBooksAdapter adapter;
+    View view;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dowloaded_books, container, false);
+        view = inflater.inflate(R.layout.fragment_dowloaded_books, container, false);
+
+        init();
+        getBooks();
+
+        return view;
     }
+
+    private void init(){
+        bookList = new ArrayList<>();
+        recyclerView_downloaded_books = view.findViewById(R.id.recyclerView_downloaded_books);
+        RecyclerView.LayoutManager mng = new GridLayoutManager(getContext(), 1);
+        recyclerView_downloaded_books.setLayoutManager(mng);
+
+
+    }
+
+    private void getBooks(){
+
+        //TODO db den kitap bilgilerini çekip listeye at aynı şekilde, AvailableBooksFragment da
+        // yaptığımız işlemin aynısı
+
+        adapter = new DownloadedBooksAdapter(bookList,getActivity(),getContext());
+        recyclerView_downloaded_books.setAdapter(adapter);
+    }
+
 }
