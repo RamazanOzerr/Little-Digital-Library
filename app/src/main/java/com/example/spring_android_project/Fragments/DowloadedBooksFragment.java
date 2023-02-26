baa9729a7c20ab7b2632f2fda44870e5f0913aa3
Ramazan Özer <ozerramazan991@gmail.com>
2023-02-27 01:41:06 +0300

    swipe to delete added (Downloaded Books - swipe left to delete)

--- app/src/main/java/com/example/spring_android_project/Fragments/DowloadedBooksFragment.java
+++ app/src/main/java/com/example/spring_android_project/Fragments/DowloadedBooksFragment.java
@@ -1,10 +1,13 @@
 package com.example.spring_android_project.Fragments;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -12,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.spring_android_project.Activities.MainActivity;
 import com.example.spring_android_project.Adapters.AvailableBooksAdapter;
 import com.example.spring_android_project.Adapters.DownloadedBooksAdapter;
 import com.example.spring_android_project.Apis.Api;
@@ -20,6 +24,8 @@ import com.example.spring_android_project.Services.BookService;
 import com.example.spring_android_project.Services.UserService;
 import com.example.spring_android_project.Utils.Book;
 import com.example.spring_android_project.Utils.User;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -31,6 +37,7 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -56,9 +63,12 @@ public class DowloadedBooksFragment extends Fragment {
         init();
         getCurrentUserId();
         swipeRefreshLayout.setOnRefreshListener(() -> {
+            bookList.clear();
             getCurrentUserId();
             swipeRefreshLayout.setRefreshing(false);
         });
+
+        swipeToRemove();
 
         return view;
     }
@@ -87,10 +97,10 @@ public class DowloadedBooksFragment extends Fragment {
 
 
         bookList.add(new Book(photoPath,name,link));
-        bookList.add(new Book(photoPath,name,link));
-        bookList.add(new Book(photoPath,name,link));
-        bookList.add(new Book(photoPath,name,link));
-        bookList.add(new Book(photoPath,name,link));
+        bookList.add(new Book(photoPath,"1",link));
+        bookList.add(new Book(photoPath,"2",link));
+        bookList.add(new Book(photoPath,"3",link));
+        bookList.add(new Book(photoPath,"4",link));
         adapter = new DownloadedBooksAdapter(bookList,getActivity(),getContext());
         recyclerView_downloaded_books.setAdapter(adapter);
 
@@ -143,4 +153,47 @@ public class DowloadedBooksFragment extends Fragment {
 
     }
 
+    private void swipeToRemove(){
+        ItemTouchHelper.SimpleCallback simpleCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                int position = viewHolder.getAdapterPosition();
+                Book deletedBook = bookList.get(position);
+                //TODO: recyclerView üzerinde silme işlemini burada yapıyoruz, db den silme de burada olmalı
+                //TODO: removeFromDb();
+                bookList.remove(position);
+                adapter.notifyItemRemoved(position);
+                Snackbar.make(recyclerView_downloaded_books, deletedBook.getBookName()
+                        , Snackbar.LENGTH_LONG)
+                        .setAction("undo", view -> {
+                            bookList.add(position,deletedBook);
+                            adapter.notifyItemInserted(position);
+                            //TODO: undoFromDb();
+                        }).show();
+
+            }
+
+            @Override
+            public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, float dX, float dY, int actionState, boolean isCurrentlyActive) {
+
+                new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                        .addBackgroundColor(ContextCompat.getColor(getContext(), R.color.red))
+                        .addActionIcon(R.drawable.baseline_delete_24)
+                        .create()
+                        .decorate();
+
+                super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+            }
+        };
+
+
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleCallback);
+        itemTouchHelper.attachToRecyclerView(recyclerView_downloaded_books);
+    }
 }
